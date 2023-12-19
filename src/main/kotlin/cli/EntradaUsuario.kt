e211c9153b82db191f971a665ac03478da6bf766
Kedap <kedap.dev@protonmail.com>
2023-12-18 21:09:32 -0600

    feat(cli/EntradaUsuario): agregando clases

---
+++ src/main/kotlin/cli/EntradaUsuario.kt
@@ -0,0 +1,13 @@
+package org.isc4151.dan.creadorManual.cli
+
+class EntradaUsuario(msg:String):AbsEntradaUsuario<String>(msg) {
+    override fun mostrar() {
+        var usuarioEntrada:String
+        do {
+            println(this.mensaje)
+            print("> ")
+            usuarioEntrada = readln()
+        } while (usuarioEntrada.isEmpty())
+        this.entrada = usuarioEntrada
+    }
+}
