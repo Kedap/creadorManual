d97d555330f63fb27e66143b6e2113ce4c845b35
Kedap <kedap.dev@protonmail.com>
2023-12-20 16:42:52 -0600

    feat(test/PracticaTest): agregando test para opciones

--- src/test/kotlin/PracticaTest.kt
+++ src/test/kotlin/PracticaTest.kt
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import java.io.File
+import java.math.BigInteger
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.security.MessageDigest
 
 class PracticaTest {
 
@@ -113,6 +115,34 @@ class PracticaTest {
         assert(File("src/test/resources/2/capturas/salidas").exists())
         assert(File("src/test/resources/2/capturas/codigos/vacia.png").exists())
         assert(File("src/test/resources/2/capturas/salidas/vacia.png").exists())
+        this.limpiar(ejemplo)
+    }
+
+    @Test
+    fun crearCapturasOpciones() {
+        val ejemplo = Practica("Prueba2", 1, "", "src/test/resources/vacia.cpp", Paths.get("src/test/resources/2"))
+        val gcc = Compilador("g++", "Gnu Compiler", null)
+        ejemplo.generarCarpetaTrab()
+        ejemplo.compilar(gcc)
+        ejemplo.crearEntradas()
+        ejemplo.ejecutar()
+        val silicon = Capturador("silicon", "silicon", null)
+        silicon.cambiarOpciones(listOf("--background", "#16f2d5"))
+        ejemplo.crearCapturas(silicon)
+        assert(File("src/test/resources/2/capturas").exists())
+        assert(File("src/test/resources/2/capturas/codigos").exists())
+        assert(File("src/test/resources/2/capturas/salidas").exists())
+        assert(File("src/test/resources/2/capturas/codigos/vacia.png").exists())
+        assert(File("src/test/resources/2/capturas/salidas/vacia.png").exists())
+
+        var capturaCodigo = Files.readAllBytes(Paths.get("src/test/resources/2/capturas/codigos/vacia.png"))
+        var hash = MessageDigest.getInstance("MD5").digest(capturaCodigo)
+        var suma = BigInteger(1,hash).toString(16)
+        assertEquals("f70ea09d2ef1d194f114c46193403159",suma)
+        capturaCodigo = Files.readAllBytes(Paths.get("src/test/resources/2/capturas/salidas/vacia.png"))
+        hash = MessageDigest.getInstance("MD5").digest(capturaCodigo)
+        suma = BigInteger(1,hash).toString(16)
+        assertEquals("bb3a5b13baf2efe3200b74c5575fdd01",suma)
         this.limpiar(ejemplo)
     }
 
