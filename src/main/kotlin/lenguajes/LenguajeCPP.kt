cd5a2135f09b9af8e12082f30615cb15f7a47a8f
Kedap <kedap.dev@protonmail.com>
2024-03-10 14:35:30 -0600

    feat(lenguajes/*CPP): agregando soporte para C++

--- src/main/kotlin/lenguajes/LenguajeCPP.kt
+++ src/main/kotlin/lenguajes/LenguajeCPP.kt
@@ -1,11 +1,22 @@
 package org.isc4151.dan.creadorManual.lenguajes
 
+import org.isc4151.dan.creadorManual.SistemaOperativo
+import org.isc4151.dan.creadorManual.obtenerOS
+import java.util.concurrent.TimeUnit
+
 class LenguajeCPP(rutaCompilador: String, opciones: List<String>) : Lenguaje(rutaCompilador, opciones) {
     override fun compilar(codigo: String, salida: String) {
-        TODO("Not yet implemented")
+        var comando = rutaCompilador
+        opciones.forEach { comando += " $it" }
+        val archivoSalida = if (obtenerOS() == SistemaOperativo.WINDOWS) "$salida.exe" else salida
+        comando += " $codigo -o $archivoSalida"
+        println("$ $comando")
+        val procesoHijo = Runtime.getRuntime().exec(comando)
+        procesoHijo.waitFor(20, TimeUnit.SECONDS)
+        if (procesoHijo.exitValue() != 0) throw Exception("Ocurrio un error al compilar")
     }
 
-    override fun obtenerEjecucion(salida: String) {
-        TODO("Not yet implemented")
+    override fun obtenerEjecucion(salida: String): String {
+        return salida
     }
 }
---
+++ src/test/kotlin/lenguajes/LenguajeCPPTest.kt
@@ -0,0 +1,53 @@
+package lenguajes
+
+import org.isc4151.dan.creadorManual.lenguajes.LenguajeCPP
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
+import java.io.File
+
+class LenguajeCPPTest {
+
+    private fun limipiar(archivo: String) {
+        File(archivo).delete()
+    }
+
+    @Test
+    fun compilar() {
+        val lenguaje = LenguajeCPP("g++", listOf())
+        val codigo = "src/test/resources/prueba.cpp"
+        val salida = "src/test/resources/prueba"
+        lenguaje.compilar(codigo, salida)
+        assert(File(salida).exists())
+        this.limipiar(salida)
+    }
+
+    @Test
+    fun compilarBanderas() {
+        val lenguaje = LenguajeCPP("g++", listOf("-Wall"))
+        val codigo = "src/test/resources/prueba.cpp"
+        val salida = "src/test/resources/prueba"
+        lenguaje.compilar(codigo, salida)
+        assert(File(salida).exists())
+        this.limipiar(salida)
+    }
+
+    @Test
+    fun compilarClang() {
+        val lenguaje = LenguajeCPP("clang++", listOf())
+        val codigo = "src/test/resources/prueba.cpp"
+        val salida = "src/test/resources/prueba"
+        lenguaje.compilar(codigo, salida)
+        assert(File(salida).exists())
+        this.limipiar(salida)
+    }
+
+    @Test
+    fun obtenerEjecucion() {
+        val lenguaje = LenguajeCPP("clang++", listOf())
+        val codigo = "src/test/resources/prueba.cpp"
+        val salida = "src/test/resources/prueba"
+        assertEquals(salida, lenguaje.obtenerEjecucion(salida))
+    }
+}
