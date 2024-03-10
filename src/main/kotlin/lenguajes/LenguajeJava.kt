0edd67d802f682597f1700d54b465f9fd91f9d27
Kedap <kedap.dev@protonmail.com>
2024-03-10 14:04:46 -0600

    refactor(lenguajes/LenguajeJava): corrigiendo nombre de las propiedades

--- src/main/kotlin/lenguajes/LenguajeJava.kt
+++ src/main/kotlin/lenguajes/LenguajeJava.kt
@@ -3,8 +3,8 @@ package org.isc4151.dan.creadorManual.lenguajes
 class LenguajeJava(
     rutaCompilador: String,
     opciones: List<String>,
-    private val rutaJavaCompilador: String,
-    private val opcionesLenguaje: List<String>
+    private val rutaJavaEjecutador: String,
+    private val opcionesEjecucion: List<String>
 ) : Lenguaje(rutaCompilador, opciones) {
     override fun compilar(codigo: String, salida: String) {
         TODO("Not yet implemented")
