06245321b26d8a9eed79f727b9363f91a9e1d926
Kedap <kedap.dev@protonmail.com>
2024-03-10 14:02:34 -0600

    refactor(Lenguaje): rutaCompilador ahora es una constante

--- src/main/kotlin/lenguajes/Lenguaje.kt
+++ src/main/kotlin/lenguajes/Lenguaje.kt
@@ -1,6 +1,6 @@
 package org.isc4151.dan.creadorManual.lenguajes
 
-abstract class Lenguaje(protected var rutaCompilador: String, protected var opciones: List<String>) {
+abstract class Lenguaje(protected val rutaCompilador: String, protected val opciones: List<String>) {
     abstract fun compilar(codigo: String, salida: String)
     abstract fun obtenerEjecucion(salida: String)
 }
