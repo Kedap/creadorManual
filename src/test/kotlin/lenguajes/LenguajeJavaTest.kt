7d3299bbbf8192dde1e68fe0993904dacd588d12
Kedap <kedap.dev@protonmail.com>
2024-03-10 18:56:56 -0600

    test(lenguajes/LenguajeJava): agregando test y recursos

---
+++ src/test/kotlin/lenguajes/LenguajeJavaTest.kt
@@ -0,0 +1,40 @@
+package lenguajes
+
+import org.isc4151.dan.creadorManual.lenguajes.LenguajeJava
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.io.File
+
+class LenguajeJavaTest {
+    private fun limpiar(archivo: String) {
+        File(archivo).delete()
+    }
+
+    @Test
+    fun compilar() {
+        val lenguaje = LenguajeJava("javac", listOf(), "java", listOf())
+        val codigo = "src/test/resources/ejemplosJava/holaMundo.java"
+        var salida = "src/test/resources/ejemplosJava/"
+        salida = lenguaje.compilar(codigo, salida)
+        assert(File(salida).exists())
+        this.limpiar(salida)
+    }
+
+    @Test
+    fun compilarBanderas() {
+        val lenguaje = LenguajeJava("javac", listOf("-source", "8"), "java", listOf())
+        val codigo = "src/test/resources/ejemplosJava/sumaDos.java"
+        var salida = "src/test/resources/ejemplosJava/"
+        salida = lenguaje.compilar(codigo, salida)
+        assert(File(salida).exists())
+        this.limpiar(salida)
+    }
+
+    @Test
+    fun obtenerEjecucion() {
+        val lenguaje = LenguajeJava("javac", listOf("-source", "8"), "java", listOf())
+        val salida = "src/test/resources/ejemplosJava/holamundo.class"
+        assertEquals("java $salida", lenguaje.obtenerEjecucion(salida))
+    }
+}
---
+++ src/test/resources/ejemplosJava/factorial.java
@@ -0,0 +1,13 @@
+// TODO: hacer que este código pida entrada de usuario
+public class factorial {
+    public static void main(String[] args) {
+        int numero = 5;
+        int factorial = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("El factorial de " + numero + " es: " + factorial);
+    }
+}
