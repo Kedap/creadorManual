5786db648e459474b912b07e371785147d81e8a9
Kedap <kedap.dev@protonmail.com>
2024-03-10 18:17:17 -0600

    feat(lenguajes/*Java): agregando soporte para Java

--- src/main/kotlin/lenguajes/LenguajeJava.kt
+++ src/main/kotlin/lenguajes/LenguajeJava.kt
@@ -1,5 +1,7 @@
 package org.isc4151.dan.creadorManual.lenguajes
 
+import java.util.concurrent.TimeUnit
+
 class LenguajeJava(
     rutaCompilador: String,
     opciones: List<String>,
@@ -7,10 +9,20 @@ class LenguajeJava(
     private val opcionesEjecucion: List<String>
 ) : Lenguaje(rutaCompilador, opciones) {
     override fun compilar(codigo: String, salida: String): String {
-        TODO("Not yet implemented")
+        var comando = rutaCompilador
+        opciones.forEach { comando+= "$ $it" }
+        comando += " $codigo -d $salida"
+        println("$ $comando")
+        val procesoHijo = Runtime.getRuntime().exec(comando)
+        procesoHijo.waitFor(20, TimeUnit.SECONDS)
+        if (procesoHijo.exitValue() != 0) throw Exception("Ocurrio un error al compilar")
+        return salida
     }
 
     override fun obtenerEjecucion(salida: String): String {
-        TODO("Not yet implemented")
+        var comando = rutaJavaEjecutador
+        opcionesEjecucion.forEach { comando += " $it" }
+        comando += salida
+        return comando
     }
 }
