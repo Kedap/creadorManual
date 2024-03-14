937571a1e4d155c545dd1b0dc31aeee5f5fbba07
Kedap <kedap.dev@protonmail.com>
2024-03-14 00:02:12 -0600

    refactor(lenguajes/FabricaLenguaje): quitando el método `configurarObtenerLenguaje()`

--- src/main/kotlin/lenguajes/FabricaLenguaje.kt
+++ src/main/kotlin/lenguajes/FabricaLenguaje.kt
@@ -1,12 +1,8 @@
 package org.isc4151.dan.creadorManual.lenguajes
 
-abstract class FabricaLenguaje() {
-    abstract var rutaCompilador: String;
-    abstract var opciones: List<String>;
-    fun configurarObtenerLenguaje(): Lenguaje {
-        configurarLenguaje();
-        return obtenerLenguaje();
-    }
+abstract class FabricaLenguaje {
+    abstract var rutaCompilador: String
+    abstract var opciones: List<String>
 
     abstract fun configurarLenguaje()
     abstract fun obtenerLenguaje(): Lenguaje;
