e9727ef14f9c821d92123c00378ddbbe1c2f57c8
Kedap <kedap.dev@protonmail.com>
2024-03-14 00:03:36 -0600

    doc(lenguajes/Lenguaje): cambiando la documentación del método compilar

--- src/main/kotlin/lenguajes/Lenguaje.kt
+++ src/main/kotlin/lenguajes/Lenguaje.kt
@@ -3,7 +3,7 @@ package org.isc4151.dan.creadorManual.lenguajes
 abstract class Lenguaje(protected val rutaCompilador: String, protected val opciones: List<String>) {
     /**
      * Se le pasa como argumento la ruta del código a compilar y se le pasa la salida el directorio en donde
-     * se va a colocar el resultado final según el lenguaje, el resultado final se llamara igual que el archivo
+     * se va a colocar el resultado final según el lenguaje, el resultado final se llamara igual que el archivo,
      * pero con la extensión según corresponda, es par ello que también se retorna
      *
      * @param codigo ruta del código fuente a compilar
